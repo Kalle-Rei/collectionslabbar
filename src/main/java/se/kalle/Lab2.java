3ec61ea57476fbd58313460da3632b3943b4a1ae
Kalle-Rei <kalle_reithmeier@hotmail.com>
2021-10-07 10:56:33 +0200

    lab2 start

---
+++ src/main/java/se/kalle/Lab2.java
@@ -0,0 +1,13 @@
+package se.kalle;
+//Uppgift:
+// Skapa en ArrayList i kod med INTS 12 ,55, 32, 9, 34.    Tips 
+//         ArrayList<Integer> tal1 = new ArrayList<>(Arrays.asList( 12,55,32,9,34));
+// Skapa en ArrayList till i kod med INTS 72 ,55, 32, 4, 88.   
+// Skapa en ArrayList - som heter commonArrayList. Denna ska nu fyllas med  med tal som återfinns i BÅDA
+// arraylistorna. Loopa igenom den första och för varje: kolla om den finns i den andra. Isåfall stoppar du in
+// den i common ArrayListen.
+// Loopa igenom common  ArrayListen och skriv ut alla tal
+// ÖVERKURS: Gör samma lab. Fast de två första ska vara ARRAYS och resultatet en ArrayList
+public class Lab2 {
+    
+}
